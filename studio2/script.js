0ec49168f9e1af440eb791ebcd6d19c9a607bf0a
Amy Lee <amylee041182@gmail.com>
2023-11-08 13:22:58 -0800

    initial html and css for studio2

--- studio2/script.js
+++ studio2/script.js
@@ -1,11 +1,11 @@
-(function(){
-    'use strict';
-    console.log('reading.js');
+// (function(){
+//     'use strict';
+//     console.log('reading.js');
 
-    $("imageSelector").click(function(e){
-        var pos = $(this).position();
-        //The following are the x/y coordinates of the mouse click relative to image.
-        var x = e.pageX - pos.left;
-        var y = e.pageY - pos.top;
-      });
-})();
+//     $("imageSelector").click(function(e){
+//         var pos = $(this).position();
+//         //The following are the x/y coordinates of the mouse click relative to image.
+//         var x = e.pageX - pos.left;
+//         var y = e.pageY - pos.top;
+//       });
+// })();
